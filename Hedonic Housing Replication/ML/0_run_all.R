7ba8656172947d41ed9020b2f89257c5ba35b6af
oliviermeslin <olivier.meslin@insee.fr>
2025-06-14 12:54:12 +0000

    Keep only one area (removing the loop on all regions)

--- Hedonic Housing Replication/ML/0_run_all.R
+++ Hedonic Housing Replication/ML/0_run_all.R
@@ -1,14 +1,8 @@
 rm(list=ls())
 
-for (j in 1:44){
-  dbase<-paste("/ssb/stamme01/wealth5/wk48/rek/prepare/housing/ml/ml_sample_",j,".csv" ,sep="")
-  dtaname<-paste("/ssb/stamme01/wealth5/wk48/rek/prepare/housing/ml/ml_result_",j,".dta" ,sep="")
-  if (j >= 42){
-    source("/home/onyxia/work/replication_HWN/Hedonic Housing Replication/ML/1_dataprep_cabin.R")
-  } else {
-    source("/home/onyxia/work/replication_HWN/Hedonic Housing Replication/ML/1_dataprep.R")
-  } 
-  source("/home/onyxia/work/replication_HWN/Hedonic Housing Replication/ML/2_fitmodel.R")
-  rm(list=ls())
-}
+dbase<-paste("/ssb/stamme01/wealth5/wk48/rek/prepare/housing/ml/ml_sample_",j,".csv" ,sep="")
+dtaname<-paste("/ssb/stamme01/wealth5/wk48/rek/prepare/housing/ml/ml_result_",j,".dta" ,sep="")
+
+source("/home/onyxia/work/replication_HWN/Hedonic Housing Replication/ML/1_dataprep.R")
+source("/home/onyxia/work/replication_HWN/Hedonic Housing Replication/ML/2_fitmodel.R")
 
