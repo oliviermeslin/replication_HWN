c33cd5c529f8863534713269ccb251e50a43cb12
oliviermeslin <olivier.meslin@insee.fr>
2025-06-14 13:04:00 +0000

    Load French data and define the holdout set

--- Hedonic Housing Replication/ML/0_run_all.R
+++ Hedonic Housing Replication/ML/0_run_all.R
@@ -1,7 +1,55 @@
 rm(list=ls())
 
-dbase<-paste("/ssb/stamme01/wealth5/wk48/rek/prepare/housing/ml/ml_sample_",j,".csv" ,sep="")
-dtaname<-paste("/ssb/stamme01/wealth5/wk48/rek/prepare/housing/ml/ml_result_",j,".dta" ,sep="")
+library(DBI)
+library(duckdb)
+library(dplyr)
+library(glue)
+
+###############################################
+# Load French data on transactions
+###############################################
+
+# Instantiate duckdb connection
+conn_ddb <- DBI::dbConnect(duckdb::duckdb())
+
+# Prepare access to S3
+dbExecute(conn_ddb, "LOAD postgres;")
+dbExecute(conn_ddb, "LOAD httpfs;")
+dbExecute(conn_ddb, "LOAD spatial;")
+
+dbExecute(conn_ddb, "SET s3_url_style = 'path';")
+
+# Load French transaction data
+dbExecute(
+  conn_ddb, 
+  glue(
+    "
+    CREATE OR REPLACE VIEW transaction_data AS
+    SELECT * from read_parquet('s3://oliviermeslin/tempFF/table_logements_vendus_final.parquet')
+    "
+  )
+)
+transaction_data <- tbl(conn_ddb, "transaction_data")
+
+###############################################
+# Define the dataset and build the holdout set
+###############################################
+
+transaction_data_flats_paris <- transaction_data |>
+  filter(ccodep == "75" & dteloc == "2") |>
+  mutate(random_number = random()) |>
+  compute("transaction_data_flats_paris", temporary = FALSE, overwrite = TRUE)
+
+# The holdout set contains 20% of the data, randomly chosen
+holdout_set <- transaction_data_flats_paris |> filter(random >= 0.8) |> collect()
+
+# The training data (dbase) contains the other 80% of the data
+dbase   <- transaction_data_flats_paris |> filter(random < 0.8) |> collect()
+dtaname <- "flats_paris"
+
+###############################################
+# Train the models
+###############################################
 
 source("/home/onyxia/work/replication_HWN/Hedonic Housing Replication/ML/1_dataprep.R")
 source("/home/onyxia/work/replication_HWN/Hedonic Housing Replication/ML/2_fitmodel.R")
